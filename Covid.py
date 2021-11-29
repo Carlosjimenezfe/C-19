dae2a56f7bd839add474f0e0cd217eb7ba8bf032
Carlos Jimenez <valuu1216@gmail.com>
2021-11-28 22:54:58 -0500

    Numero de Contagios

--- Covid.py
+++ Covid.py
@@ -10,7 +10,7 @@ url = 'covid_22_noviembre.csv'
 data = pd.read_csv(url)
 
 """
-Número de casos de Contagiados en el País.
+1. Número de casos de Contagiados en el País.
 """
- n_casos = data.shape[0]
-print(f'El numero de contagiado en Colombi es de: {n_casos}')
+n_Casos = data.shape[0]
+print(f'El numero de casos de contagios en Colombia es de: {n_Casos}')
